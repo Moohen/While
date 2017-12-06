08f73c454065101e461eace319a478219316cd61
Moohen <matt.moohen@gmail.com>
2017-12-06 14:19:33 +0100

    Simple examples of while loop

--- src/com/moohen/Main.java
+++ src/com/moohen/Main.java
@@ -10,5 +10,13 @@ public class Main {
             count++;
         }
 
+        count = 1;
+        while (true) {
+            if (count == 6) {
+                break;
+            }
+            System.out.println("Count value is " + count);
+            count++;
+        }
     }
 }
