8a44903c8dd9f9a0fe33c60efb862e4db9f6925a
Moohen <matt.moohen@gmail.com>
2017-12-06 14:11:53 +0100

    Initial commit

---
+++ src/com/moohen/Main.java
@@ -0,0 +1,14 @@
+package com.moohen;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int count = 1;
+        while (count != 6) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+
+    }
+}
